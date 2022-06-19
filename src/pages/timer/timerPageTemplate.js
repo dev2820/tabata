c2c200a3e47320490e7849d6125bbcdae52df9da
dev2820 <second28200@gmail.com>
2022-06-20 01:28:09 +0900

    refactor/ semantic한 요소 사용을 위해 main 추가

--- src/pages/timer/timerPageTemplate.js
+++ src/pages/timer/timerPageTemplate.js
@@ -2,12 +2,14 @@ const template = document.createElement("template");
 template.className = "template timer-page";
 template.innerHTML = `
 <style></style>
-<section class="timer-zone">
-    <my-timer class="timer" is-run="true"></my-timer>
-    <my-reps></my-reps>
-    <run-and-stop-button class="run-and-stop"></run-and-stop-button>
-</section>
-
+<main>
+    <section class="timer-zone">
+        <h2 class="phase"></h2>
+        <my-timer class="timer"></my-timer>
+        <my-reps></my-reps>
+        <run-and-stop-button class="run-and-stop"></run-and-stop-button>
+    </section>
+</main>
 `;
 
 export default template;
