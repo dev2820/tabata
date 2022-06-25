e49a7bfe700e0022ad61d7d71e90f0634f425b1f
dev2820 <second28200@gmail.com>
2022-06-25 23:45:53 +0900

    refactor/ phase클래스에 time 정보 추가
    phase마다 얼만큼의 time을 사용하는지 저장하고 timer 컴포넌트 등에서
    불러와서 사용하게 함
    
    이를 통해 나중엔 세밀한 시간 조절도 가능할 것으로 보임

--- src/modules/phase.js
+++ src/modules/phase.js
@@ -1,32 +1,36 @@
+import Time from "./Time";
+
 export default class Phase {
   reps = 0;
   name = "";
-  constructor(phaseName, reps) {
+  time = new Time({ sec: 0 });
+  constructor(phaseName, reps, time) {
     this.name = phaseName;
     this.reps = reps;
+    this.time = time;
   }
 }
 
 export class StartPhase extends Phase {
   constructor(reps) {
-    super("start", reps);
+    super("start", reps, new Time({ sec: 0 }));
   }
 }
 
 export class RunPhase extends Phase {
-  constructor(reps) {
-    super("run", reps);
+  constructor(reps, time) {
+    super("run", reps, time);
   }
 }
 
 export class BreakPhase extends Phase {
-  constructor(reps) {
-    super("break", reps);
+  constructor(reps, time) {
+    super("break", reps, time);
   }
 }
 
 export class EndPhase extends Phase {
   constructor(reps) {
-    super("end", reps);
+    super("end", reps, new Time({ sec: 0 }));
   }
 }
