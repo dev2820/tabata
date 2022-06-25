2ae5b42336ab4277578cb7480744b4bd2e955f66
dev2820 <second28200@gmail.com>
2022-06-25 22:05:42 +0900

    feature/ exercise store 추가
    exercise를 클래스 instance로 관리했지만, 다른 컴포넌트에서도
    접근해야할일이 많다.
    
    이전에는 timerPage에서 dispatch를 통해 exercise의 변경을 알렸지만, 전역
    상태를 통해 모든 컴포넌트에서 접근할 수 있게 하는 것이 더 편리할 것이라
    생각 되어 exercise 컴포넌트를 생성했다

---
+++ src/stores/exercise.js
@@ -0,0 +1,111 @@
+import Time from "../modules/Time";
+import { RunPhase, BreakPhase, StartPhase, EndPhase } from "../modules/phase";
+//event == action 이다
+let deepCopy = (obj) => {
+  return Object.assign(Object.create(Object.getPrototypeOf(obj)), obj);
+};
+const INITIAL_STATE = {
+  runTime: new Time({ min: 0, sec: 5 }),
+  breakTime: new Time({ min: 0, sec: 3 }),
+  goal: 0,
+  phaseList: [],
+  currentPhaseIndex: 0,
+  get currentPhase() {
+    return this.phaseList[this.currentPhaseIndex];
+  },
+  get currentReps() {
+    return currentPhase.reps;
+  },
+  get isStart() {
+    return this.currentPhase instanceof StartPhase;
+  },
+  get isRun() {
+    return this.currentPhase instanceof RunPhase;
+  },
+  get isBreak() {
+    return this.currentPhase instanceof BreakPhase;
+  },
+  get isEnd() {
+    return this.currentPhase instanceof EndPhase;
+  },
+};
+const makePhaseList = (goal) => {
+  if (goal < 1) return null;
+
+  let reps = 0;
+  const list = [];
+  list.push(new StartPhase(reps));
+  reps++;
+  for (let i = 0; i < goal - 1; i++) {
+    list.push(new RunPhase(reps));
+    list.push(new BreakPhase(reps));
+    reps++;
+  }
+  list.push(new RunPhase(reps));
+  list.push(new EndPhase(reps));
+
+  return list;
+};
+const initExercise = (state, action) => {
+  const { runTime, breakTime, goal } = action.payload;
+  if (!goal) {
+    return state;
+  }
+
+  const newPhaseList = makePhaseList(goal);
+  return {
+    ...state,
+    runTime,
+    breakTime,
+    goal,
+    newPhaseList,
+  };
+};
+const nextPhase = (state, action) => {
+  const nextIndex = state.currentPhaseIndex + 1;
+  if (nextIndex >= state.phaseList.length) {
+    return state;
+  }
+
+  return {
+    ...state,
+    currentPhaseIndex: nextIndex,
+  };
+};
+
+const prevPhase = (state, action) => {
+  const prevIndex = state.currentPhaseIndex - 1;
+  if (prevIndex < 0) {
+    return state;
+  }
+
+  return {
+    ...state,
+    currentPhaseIndex: prevIndex,
+  };
+};
+
+const methods = Object.freeze({
+  INIT_EXERCISE: initExercise,
+  NEXT_PHASE: nextPhase,
+  PREV_PHASE: prevPhase,
+});
+
+export const EVENT_TYPES = Object.freeze({
+  INIT_EXERCISE: "INIT_EXERCISE",
+  NEXT_PHASE: "NEXT_PHASE",
+  PREV_PHASE: "PREV_PHASE",
+});
+
+export default (initialState = INITIAL_STATE) => {
+  return (state, action) => {
+    if (!state) {
+      return deepCopy(initialState);
+    }
+    const currentMethod = methods[action.type];
+    if (!currentMethod) {
+      return state;
+    }
+    return currentMethod(state, action);
+  };
+};
