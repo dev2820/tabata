9299ea8a04ac92440455bc5613531b75827f2bce
dev2820 <second28200@gmail.com>
2022-06-25 19:02:01 +0900

    feature/ eventbus 추가
    eventbus 기반의 상태관리모듈 eventbus 추가
    
    redux처럼 독립적으로 동작할 수 있도록 작성
    Component 클래스에서 store로 등록해주면 사용할 수 있음
    
    globalStore 등록 후 전역 상태 관리로 이용할 예정

---
+++ eventbus.js
@@ -0,0 +1,52 @@
+let deepCopy = (obj) => {
+  return Object.assign(Object.create(Object.getPrototypeOf(obj)), obj);
+};
+
+const freeze = (obj) => {
+  return Object.freeze(deepCopy(obj));
+};
+
+export default (model) => {
+  let listeners = [];
+  let state = model();
+
+  // 이벤트 등록
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      // unsubscriber
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  //???
+  const invokeSubscribers = () => {
+    listeners.forEach((l) => l());
+  };
+
+  // 이벤트 발생
+  const dispatch = (event) => {
+    const newState = model(state, event); //이벤트를 적용해 새로운 state를 생성
+
+    if (!newState) {
+      throw new Error("model should always return a value");
+    }
+
+    if (newState === state) {
+      //새로운 state와 기존 state가 같다면 변화 x
+      return;
+    }
+
+    state = newState; //새로운 state를 적용
+
+    invokeSubscribers(); //state에 따른 리스너 실행
+  };
+
+  return {
+    listeners,
+    subscribe,
+    dispatch,
+    getState: () => freeze(state),
+  };
+};
