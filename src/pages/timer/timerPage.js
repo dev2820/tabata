01c7d9726a06b8a3462a5830832a49ddbee24589
dev2820 <second28200@gmail.com>
2022-06-26 00:06:30 +0900

    refactor/ 사용되지 않는 dispatchEvent 정리

--- src/pages/timer/timerPage.js
+++ src/pages/timer/timerPage.js
@@ -45,81 +45,19 @@ export default class TimerPage extends Component {
           .getCurrentPhase().name;
 
         newDOM
-          .querySelector("run-and-stop-button")
-          .addEventListener(EVENT.CLICK, this.methods.timerToggle);
+          .querySelector("button.run-and-stop")
+          .addEventListener("click", this.methods.timerToggle);
         return newDOM;
       },
       store: {
         exercise: useGlobalStore("exercise"),
       },
       state: {
-        runTime: new Time({ min: 0, sec: 5 }),
-        breakTime: new Time({ min: 0, sec: 3 }),
-        exercise: new Exercise(3),
+        isRun: false,
       },
-      created() {
-        this.addEventListener(EVENT.TIMEOVER, this.methods.timeoverHandler);
-        this.setState({
-          exercise: this.state["exercise"].next(),
-        });
-        this.$("my-timer").dispatchEvent(
-          new CustomEvent(EVENT.SETTIME, {
-            detail: { time: this.state["runTime"] },
-          })
-        );
-        this.$("my-timer").dispatchEvent(new Event(EVENT.RUN));
-        this.$("my-reps").dispatchEvent(
-          new CustomEvent(EVENT.SETREPS, {
-            detail: {
-              current: this.state["exercise"].phase.reps,
-              goal: this.state["exercise"].goal,
-            },
-          })
-        );
-      },
-      connected() {},
       methods: {
-        timeoverHandler(e) {
-          this.setState({
-            exercise: this.state["exercise"].next(),
-          });
-          if (this.state["exercise"].phase.name === "end") {
-            new CustomEvent(EVENT.SETREPS, {
-              detail: {
-                current: this.state["exercise"].phase.reps,
-                goal: this.state["exercise"].goal,
-              },
-            });
-          } else if (this.state["exercise"].phase.name === "run") {
-            this.$("my-timer").dispatchEvent(
-              new CustomEvent(EVENT.SETTIME, {
-                detail: {
-                  time: this.state["runTime"],
-                },
-              })
-            );
-            this.$("my-reps").dispatchEvent(
-              new CustomEvent(EVENT.SETREPS, {
-                detail: {
-                  current: this.state["exercise"].phase.reps,
-                  goal: this.state["exercise"].goal,
-                },
-              })
-            );
-            this.$("my-timer").dispatchEvent(new Event(EVENT.RUN));
-          } else if (this.state["exercise"].phase.name === "break") {
-            this.$("my-timer").dispatchEvent(
-              new CustomEvent(EVENT.SETTIME, {
-                detail: {
-                  time: this.state["breakTime"],
-                },
-              })
-            );
-            this.$("my-timer").dispatchEvent(new Event(EVENT.RUN));
-          }
-        },
         timerToggle(e) {
-          const isTimerRun = this.state["exercise"].isRun;
+          const isTimerRun = this.state["isRun"];
           if (isTimerRun) {
             this.methods.timerStop();
           } else {
@@ -129,20 +67,10 @@ export default class TimerPage extends Component {
         timerStop() {
           this.state["exercise"].stop();
           this.$("my-timer").dispatchEvent(new Event(EVENT.STOP));
-          this.$("run-and-stop-button.run-and-stop").dispatchEvent(
-            new CustomEvent(EVENT.CHANGE_STATE, {
-              detail: { state: "stop" },
-            })
-          );
         },
         timerContinue() {
           this.state["exercise"].continue();
           this.$("my-timer").dispatchEvent(new Event(EVENT.RUN));
-          this.$("run-and-stop-button.run-and-stop").dispatchEvent(
-            new CustomEvent(EVENT.CHANGE_STATE, {
-              detail: { state: "run" },
-            })
-          );
         },
       },
     });
