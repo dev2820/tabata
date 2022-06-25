7facdf6f27818fb43eb1b7c30dcff65bc9ebca78
dev2820 <second28200@gmail.com>
2022-06-25 19:01:05 +0900

    refactor/ router.js -> router/index.js로 이동
    이유는 딱히 없음, 폴더별로 정리하려고 함

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+export default {
+  container: document.querySelector("div#app"),
+  mode: "history",
+  routes: [
+    { path: "/", component: () => document.createElement("timer-page") },
+    {
+      path: "/test/:id",
+      component: () => document.createElement("regex-page"),
+    },
+    { path: "/404", component: () => document.createElement("not-found-page") },
+    // { path: "*", redirect: "/404" },
+  ],
+};
