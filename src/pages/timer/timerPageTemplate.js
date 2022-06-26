8d5988400fe51e1529f722d46c78bb9016b8dfa5
dev2820 <second28200@gmail.com>
2022-06-26 15:12:00 +0900

    style/ 다음,이전 버튼 추가

--- src/pages/timer/timerPageTemplate.js
+++ src/pages/timer/timerPageTemplate.js
@@ -15,7 +15,23 @@ template.innerHTML = `
             <h2 class="phase"></h2>
             <my-timer class="timer"></my-timer>
             <my-reps></my-reps>
-            <button class="run-and-stop">정지</button>
+            <div class="controller">
+              <button class="prev">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevrons-left" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <polyline points="11 7 6 12 11 17" />
+                <polyline points="17 7 12 12 17 17" />
+                </svg>
+              </button>
+                <button class="run-and-stop">정지</button>
+                <button class="next">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevrons-right" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <polyline points="7 7 12 12 7 17" />
+                    <polyline points="13 7 18 12 13 17" />
+                </svg>
+              </button>
+            </div>
         </section>
     </main>
 </div>
